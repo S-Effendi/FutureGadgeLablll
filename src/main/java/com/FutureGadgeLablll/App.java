2a6396c25d750c2f8e8791d111af2ff1f72478cc
S-Effendi <s.effendi93@gmail.com>
2018-11-26 11:07:44 +0200

    New Project Version lll

---
+++ src/main/java/com/FutureGadgeLablll/App.java
@@ -0,0 +1,13 @@
+package com.FutureGadgeLablll;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
